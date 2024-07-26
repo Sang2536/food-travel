8bfd07a0a7d87ec853e88ea41367c9bfe0c14ed5
Sang-2000 <dovansang2536@gmail.com>
2024-07-26 20:17:38 +0700

    restructure the index section - part product

--- app/Models/Casts/PhotoUrlCustomCast.php
+++ app/Models/Casts/PhotoUrlCustomCast.php
@@ -3,6 +3,7 @@ namespace App\Models\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Casts\Attribute as CastsAttribute;
+use Illuminate\Support\Facades\Storage;
 
 class PhotoUrlCustomCast implements CastsAttributes {
     public function set($model, $key, $value, $attributes)
---
+++ resources/views/products/partials/index_statistical.blade.php
@@ -0,0 +1,37 @@
+<div class="my-8 mx-2 rounded bg-gray-50 text-gray-900 dark:text-white">
+    <div class="grid grid-cols-3 divide-x-2">
+        <div class="m-2 p-2 text-center max-w-sm bg-red-200 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex items-center text-gray-900 whitespace-nowrap dark:text-white">
+                <div class="flex w-1/6 h-full items-center justify-center text-sm font-medium text-white focus:ring-4 focus:outline-none dark:focus:ring-green-800">
+                    icon
+                </div>
+                <div class="ps-3">
+                    <div class="font-normal text-gray-500">Product</div>
+                    <div class="text-2xl text-start font-semibold text-gray-700 dark:text-gray-400">1243</div>
+                </div>
+            </div>
+        </div>
+        <div class="m-2 p-2 text-center max-w-sm bg-red-200 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex items-center text-gray-900 whitespace-nowrap dark:text-white">
+                <div class="flex w-1/6 h-full items-center justify-center text-sm font-medium text-white focus:ring-4 focus:outline-none dark:focus:ring-green-800">
+                    icon
+                </div>
+                <div class="ps-3">
+                    <div class="font-normal text-gray-500">Product</div>
+                    <div class="text-2xl text-start font-semibold text-gray-700 dark:text-gray-400">1243</div>
+                </div>
+            </div>
+        </div>
+        <div class="m-2 p-2 text-center max-w-sm bg-red-200 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex items-center text-gray-900 whitespace-nowrap dark:text-white">
+                <div class="flex w-1/6 h-full items-center justify-center text-sm font-medium text-white focus:ring-4 focus:outline-none dark:focus:ring-green-800">
+                    icon
+                </div>
+                <div class="ps-3">
+                    <div class="font-normal text-gray-500">Product</div>
+                    <div class="text-2xl text-start font-semibold text-gray-700 dark:text-gray-400">1243</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
